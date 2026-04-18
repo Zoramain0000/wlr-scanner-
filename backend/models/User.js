9b344df4bf7b4400f796787b20dd2964af448986
Zoramain0000 <zoragonzales3@gmail.com>
2026-04-18 22:30:22 +0800

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,9 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    callsign: { type: String, unique: true, required: true, uppercase: true },
-    email: { type: String, unique: true, required: true, lowercase: true },
-    password: { type: String, required: true },
-    name: String,
-    verified: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now }
-});
-
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 12);
-    next();
-});
+  callsign: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
